458f532f8b9e2a812f859ab04283d1ecc59fa994
Brian Wright <bwright@webstaurantstore.com>
2021-02-18 23:47:04 -0500

    testing section

--- guessing_game.py
+++ guessing_game.py
@@ -1,34 +1,60 @@
 import random
 import sys
 
-try:
-    start = sys.argv[1] or 1
-except:
-    start = 1
-    print(f"Updating starting number to a positive integer: {start}.")
-try:
-    stop = sys.argv[2] or 10
-except:
-    stop = start + 10
-    print(f"Updating stopping number to a positive integer: {stop}.")
 
-if int(start) < 0:
-    start = 1
-    print(f"Updating starting number to a positive integer: {start}.")
-if int(stop) < 0:
-    stop = int(start) + 10
-    print(f"Updating stopping number to a positive integer: {stop}.")
+def evaluateGuess(guess, number):
+    if guess == number:
+        print("Congratulations, you got it!")
+        return True
+    else:
+        print("Try again!")
 
-number = random.randint(int(start), int(stop))
+    return False
 
-print()
-print("Welcome to the guessing game!")
-print(f"I'm thinking of a number between {start} and {stop}, try and guess it!")
-print()
 
-guess = None
-while guess != number and guess != -1:
-    guess = int(input("Please enter a guess! (-1 to quit): "))
+def setupStartAndStop():
+    try:
+        start = sys.argv[1]
+    except:
+        start = 1
+        print(f"Updating starting number to a positive integer: {start}.")
+    try:
+        stop = sys.argv[2]
+    except:
+        stop = start + 10
+        print(f"Updating stopping number to a positive integer: {stop}.")
 
-print(
-    "Congratulations, you got it!" if guess == number else f"Sorry to see you go, try again next time! P.S. It was {number}")
+    if int(start) < 0:
+        start = 1
+        print(f"Updating starting number to a positive integer: {start}.")
+    if int(stop) < 0:
+        stop = int(start) + 10
+        print(f"Updating stopping number to a positive integer: {stop}.")
+    return start, stop
+
+
+def promptUser(start, stop):
+    print()
+    print("Welcome to the guessing game!")
+    print(f"I'm thinking of a number between {start} and {stop}, try and guess it!")
+    print()
+
+
+def main():
+    start, stop = setupStartAndStop()
+    promptUser(start, stop)
+    number = random.randint(int(start), int(stop))
+
+    while True:
+        guess = int(input("Please enter a guess! (-1 to quit): "))
+
+        if evaluateGuess(guess, number):
+            break
+
+        if guess == -1:
+            print(f"Sorry to see you go, try again next time! P.S. It was {number}")
+            break
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_guessing_game.py
@@ -0,0 +1,15 @@
+import unittest
+import guessing_game
+
+
+class TestGuessingGame(unittest.TestCase):
+
+    def test_evaluateGuess(self):
+        self.assertTrue(guessing_game.evaluateGuess(1, 1))
+
+    def test_evaluateGuessFalse(self):
+        self.assertFalse(guessing_game.evaluateGuess(2, 1))
+
+
+if __name__ == '__main__':
+    unittest.main()
